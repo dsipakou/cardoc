9015383b378a4d10140824d864e0d7c532e4fc96
ror <ror@ror-virtual-machine.(none)>
2014-01-30 12:50:21 +0300

    first commit

---
+++ app/controllers/index_controller.rb
@@ -0,0 +1,5 @@
+class IndexController < ApplicationController
+  layout 'frontend'
+  def index
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cardoc::Application.config.secret_token = '36a8b0d83909215f6da0c5a8ae884d9686dfd91231b4e3b512aefd1262235d6a3ffd0c21066fcf6f7f9f8d09e9cb0c7bf4c31c55764c917fb343305d76b2f258'
